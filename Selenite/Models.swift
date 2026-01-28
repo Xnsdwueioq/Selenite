d3276724397e016f84a20656df8cf6ab92be0900
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-01-28 23:11:31 +0700

    feat: add Session and SessionInterval models

---
+++ Selenite/Models.swift
@@ -0,0 +1,67 @@
+//
+//  Models.swift
+//  Selenite
+//
+//  Created by Eyhciurmrn Zmpodackrl on 28.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+
+enum SessionType: String, Codable {
+  case active
+  case solid
+  case fragmented
+}
+
+@Model
+final class SessionInterval {
+  var startTime: Date
+  var endTime: Date?
+  var calendarEventID: String?
+  
+  var duration: TimeInterval {
+    startTime.distance(to: endTime ?? Date())
+  }
+  
+  init(startTime: Date = Date(), endTime: Date? = nil, calendarEventID: String? = nil) {
+    self.startTime = startTime
+    self.endTime = endTime
+    self.calendarEventID = calendarEventID
+  }
+}
+
+@Model
+final class Session {
+  var title: String
+  var sessionType: SessionType
+  
+  @Relationship(deleteRule: .cascade)
+  var intervals: [SessionInterval]
+  
+  var sessionDuration: TimeInterval {
+    intervals.reduce(into: 0) { totalSum, interval in
+      totalSum += interval.duration
+    }
+  }
+  var interruptionsDuration: TimeInterval {
+    guard intervals.count >= 2 else { return 0 }
+    let sortedIntervals = intervals.sorted(by: { $0.startTime < $1.startTime })
+    
+    var totalDuration: TimeInterval = 0
+    for i in 0..<(sortedIntervals.count - 1) {
+      guard let pauseStartTime: Date = sortedIntervals[i].endTime else { continue }
+      let pauseEndTime: Date = sortedIntervals[i + 1].startTime
+      totalDuration += pauseStartTime.distance(to: pauseEndTime)
+    }
+    
+    return totalDuration
+  }
+  
+  init(title: String = "", sessionType: SessionType = SessionType.active, intervals: [SessionInterval] = []) {
+    self.title = title
+    self.sessionType = sessionType
+    self.intervals = intervals
+  }
+}
