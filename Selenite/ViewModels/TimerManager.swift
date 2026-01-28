5667bd2fbe076d55bc4d3acab23cd196b7d7255e
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-01-29 01:02:40 +0700

    feat(models): add TimerManager, that can control timer state
    wip

--- Selenite/ViewModels/TimerManager.swift
+++ Selenite/ViewModels/TimerManager.swift
@@ -11,5 +11,45 @@ import SwiftUI
 
 @Observable
 final class TimerManager {
-  var activeSession: 
+  var activeSession: Session?
+  
+  var pulse: Bool = false
+  
+  private var timer: Timer?
+  
+  func startNewSession(modelContext: ModelContext) {
+    let newSession = Session(title: "Selenite")
+    let firstInterval = SessionInterval(startTime: Date())
+    
+    newSession.intervals.append(firstInterval)
+    activeSession = newSession
+
+    modelContext.insert(newSession)
+    
+    startPulse()
+  }
+  
+  func resume() {
+    let nextInterval = SessionInterval(startTime: Date())
+    activeSession?.intervals.append(nextInterval)
+    startPulse()
+  }
+  
+  func pause() {
+    if let lastInterval = activeSession?.intervals.last, lastInterval.endTime == nil {
+      lastInterval.endTime = Date()
+    }
+    stopPulse()
+  }
+  
+  private func startPulse() {
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] _ in
+      self?.pulse.toggle()
+    })
+  }
+  
+  private func stopPulse() {
+    timer?.invalidate()
+    timer = nil
+  }
 }
